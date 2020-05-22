bcac83f92ee86856d39f99ad41ddc4d67d52f9c6
Chris <chrisliupascal@gmail.com>
2020-05-22 16:32:40 +0800

    Update css

--- src/pages/workneducation.js
+++ src/pages/workneducation.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Root from "../components/root/Root";
+import { VerticalTimeline }  from 'react-vertical-timeline-component';
 
 const WorkAndEducationPage = () => (
   <Root>
-    <div className="main-content-skills">
-      <h1 className="skills-header">
-        <span>Started coding since 2013.</span>
+    <div className="main-content-wne">
+      <h1 className="wne-header">
+        <span>Work Experience & Education</span>
       </h1>
-      <p className="skills-subheader">
-        Now mainly focus on <strong>Web Development</strong>.
-      </p>
+      <VerticalTimeline>
+        <div className="vertical-timeline-element--work vertical-timeline-element">
+          <span
+            className="vertical-timeline-element-icon bounce-in"
+            style={{ background: 'rgb(33, 150, 243)', color: 'rgb(255, 255, 255)' }}
+          />
+          <div className="vertical-timeline-element-content bounce-in">
+            <div className="vertical-timeline-element-content-arrow" />
+            <h2 className="timeline-element-title">Assistant Software Developer</h2>
+            <h4>HKUST</h4>
+            <p className="timeline-element-description">
+              Designed and implemented frontend interface for the Online Assessment Platform
+            </p>
+            <span className="vertical-timeline-element-date">July 2019 - Present</span>
+          </div>
+        </div>
+      </VerticalTimeline>
     </div>
   </Root>
 );
