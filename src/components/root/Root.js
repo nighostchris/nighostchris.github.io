79aa769f93cdeeffe1c7803cc79bcd0559431381
Chris <chrisliupascal@gmail.com>
2020-05-23 23:32:54 +0800

    Add a responsive top bar

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./root.css";
 import SideBar from "./SideBar";
+import TopBar from "./TopBar";
 import PropTypes from "prop-types";
 
 const Root = ({ children }) => {
   return (
     <div className="root">
       <SideBar />
+      <TopBar />
       <div className="main-content">
         { children }
       </div>
---
+++ src/components/root/TopBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import "./topbar.css";
+
+const TopBar = () => {
+  const [openHamburger, setOpenHamburger] = React.useState(false);
+
+  return (
+    <div className="topbar">
+      <div className="topbar-hamburger">
+        <i
+          className="fas fa-bars hamburger"
+          onClick={() => setOpenHamburger(!openHamburger)}
+        />
+      </div>
+      <div className="topbar-title-wrapper">
+        <a href="/" style={{ textDecoration: "none" }}>
+          <h1 className="topbar-title">{`Chris  -  Liu`}</h1>
+        </a>
+      </div>
+      <menu className="topbar-menu" style={{ display: openHamburger ? 'flex' : 'none' }}>
+        <nav>
+          <ul className="sidebar-ul">
+            {
+              ["home", "skills", "projects", "work & edu"].map((tab, index) => (
+                <li className="sidebar-li">
+                  <a
+                    className="sidebar-li-a"
+                    href={`/${index === 0 ? "" : (index === 3 ? "workneducation" : tab)}`}
+                  >
+                    {tab}
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+      </menu>
+    </div>
+  );
+};
+
+export default TopBar;
