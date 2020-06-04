d769f2e53134691a53c731424dfb936c7d152be9
Chris <chrisliupascal@gmail.com>
2020-06-04 21:41:43 +0800

    Update some font family on some paragraph

--- src/html.js
+++ src/html.js
@@ -6,6 +6,7 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         <link href="https://fonts.googleapis.com/css?family=Rubik:400,500|Roboto+Mono:400,500" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
         <link href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" rel="stylesheet" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
