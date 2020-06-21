a6a6f095e5c777cb3ab50306d80b98078e5bf23a
Chris <chrisliupascal@gmail.com>
2020-06-21 14:20:25 +0800

    Added a code selector bar for my convenience

--- src/pages/writepassage.js
+++ src/pages/writepassage.js
@@ -13,16 +13,46 @@ import "./writepassage.css";
 
 const WritePassagePage = () => {
   const [rawPassage, setRawPassage] = React.useState("");
+
   return (
     <Root>
       <Helmet>
         <title>Chris Liu - Write Passage</title>
       </Helmet>
       <div className="main-content-write-passage">
-        <textarea
-          className="passage-editor"
-          value={rawPassage}
-          onChange={(e) => setRawPassage(e.target.value)} />
+        <div className="write-passage-left-panel">
+          <div className="jsx-block-selector">
+            <i
+              className="fas fa-paragraph jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<NormalText></NormalText>")}
+            />
+            <i
+              className="fas fa-bold jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<BoldText></BoldText>")}
+            />
+            <i
+              className="fas fa-link jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<HyperLinkText href={``}></HyperLinkText>")}
+            />
+            <i
+              className="fas fa-heading jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<Header></Header>")}
+            />
+            <i
+              className="fas fa-code jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<Code language=\"\"></Code>")}
+            />
+            <i
+              className="fas fa-file-image jsx-block-icon"
+              onClick={() => setRawPassage(rawPassage + "<Image src=\"\" alt=\"\" />")}
+            /> 
+          </div>
+          <textarea
+            className="passage-editor"
+            value={rawPassage}
+            onChange={(e) => setRawPassage(e.target.value)}
+          />
+        </div>
         <Container>
           <JsxParser
             components={{ Title, Subtitle, Details, BoldText, NormalText, Header, Code, HyperLinkText, Image, ButtonLink }}
